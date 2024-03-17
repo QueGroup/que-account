88cfeaea09128f6f5dce130cc943c6f6af0803bc
dromanov <dtdzhalaev@gmail.com>
2024-03-17 20:50:47 +0300

    🗃️ add models

---
+++ src/infrastructure/database/models/__init__.py
@@ -0,0 +1,23 @@
+from .base import (
+    Base,
+)
+from .interest import (
+    InterestModel,
+)
+from .profile import (
+    ProfileModel,
+)
+from .role import (
+    RoleModel,
+)
+from .user import (
+    UserModel,
+)
+
+__all__ = (
+    "Base",
+    "UserModel",
+    "RoleModel",
+    "InterestModel",
+    "ProfileModel",
+)
---
+++ src/infrastructure/database/models/role.py
@@ -0,0 +1,49 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Table,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from .base import (
+    Base,
+)
+
+if TYPE_CHECKING:
+    from .user import (
+        UserModel,
+    )
+
+roles_to_user = Table(
+    "roles_to_user",
+    Base.metadata,
+    Column(
+        "user_id", Integer, ForeignKey("users.user_id", ondelete="CASCADE", onupdate="CASCADE"),
+        primary_key=True,
+    ),
+    Column(
+        "role_id", Integer, ForeignKey("roles.role_id", ondelete="CASCADE", onupdate="CASCADE"),
+        primary_key=True,
+    )
+)
+
+
+class RoleModel(Base):
+    role_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+
+    users: Mapped[list["UserModel"]] = relationship(
+        "UserModel",
+        back_populates="roles",
+        secondary=roles_to_user,
+    )

591a3000d36a4694de044328823926b7848697ab
dromanov <dtdzhalaev@gmail.com>
2024-03-22 15:09:22 +0300

    ✨ add validator for pswd

--- src/application/dto/auth_dto.py
+++ src/application/dto/auth_dto.py
@@ -30,11 +30,27 @@ class UserRegistrationSchema(BaseModel):
     password: str | None = None
 
     @field_validator("username")
-    def validate_name(cls, value) -> str:
+    def validate_name(cls, value: str) -> str:
         match_pattern = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
         if not match_pattern.match(value):
             raise HTTPException(
                 status_code=422, detail="Name should contains only letters"
+            )
+        return value
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        if value and not len(value) < 8:
+            raise HTTPException(
+                status_code=422, detail="Password should be at least 8 characters long"
+            )
+        if value.islower() or value.isupper():
+            raise HTTPException(
+                status_code=422, detail="Password should contain both uppercase and lowercase letters"
+            )
+        if value.isalnum():
+            raise HTTPException(
+                status_code=422, detail="Password should contain at least one special character"
             )
         return value
 

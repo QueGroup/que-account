c48c4e3f9393c92129758dd03958689a4b7a231c
dromanov <dtdzhalaev@gmail.com>
2024-05-20 01:38:53 +0300

    💡 improve error message

--- src/presentation/api/exceptions.py
+++ src/presentation/api/exceptions.py
@@ -74,7 +74,7 @@ class UserDeactivatedError(HTTPException):
     def __init__(
             self,
             status_code: int = status.HTTP_400_BAD_REQUEST,
-            message: str = "User deactivated"
+            message: str = "Your account is deactivated"
     ):
         detail = {"code": ex.AuthExceptionCodes.USER_DEACTIVATED, "message": message}
         super().__init__(status_code=status_code, detail=detail)
